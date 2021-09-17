3414be7e704ef58c189c0fd665373c2fc7aa6e60
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:49:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Run this code block 3 and examine the error message.
+# Pay close attention to any line numbers mentioned in the message - these will help locate and repair the mistake in the code.
+
+# Code block 3:
+launch_ready = False
+fuel_level = 27000
+
+if fuel_Level >= 20000:
+  print('Fuel level cleared.')
+  launch_ready = True
+else:
+  print('WARNING: Insufficient fuel!')
+  launch_ready = False
+
+# Code block 4:
+# Un-comment lines 18 - 26, run the program, and fix the error.
+
+# if launch_ready:
+#     print("10, 9, 8...")
+#     print("Fed parrot...")
+#     print("6, 5, 4...")
+#     print("Ignition...")
+#     printt("3, 2, 1...")
+#     print("Liftoff!")
+# else:
+#     print("Launch scrubbed.")
